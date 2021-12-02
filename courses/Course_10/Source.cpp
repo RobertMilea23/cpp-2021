b89eb921ec730e22585e17845a75d55f9c6c0195
Catalin <boja.catalin@hotmail.com>
2021-12-02 13:35:04 +0200

    Week 10 update

--- courses/Course_10/Source.cpp
+++ courses/Course_10/Source.cpp
@@ -96,6 +96,52 @@ public:
 		this->addGrade(newGrade);
 	}
 
+	bool operator>=(GradeBook& gradeBook) {		//we use the & to avoid the copy ctor
+		if (this->noGrades >= gradeBook.noGrades) {
+			return true;
+		}
+		else {
+			return false;
+		}
+
+		//return this->noGrades >= gradeBook.noGrades
+	}
+
+	bool operator!() {
+		if (this->noGrades == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	explicit operator float() {
+		int sum = 0;
+		for (int i = 0; i < this->noGrades; i++) {
+			sum += this->grades[i];
+		}
+		if (this->noGrades == 0) {
+			return 0;
+		}
+		else {
+			return (float) sum / this->noGrades;
+		}
+	}
+
+	//pre
+	GradeBook operator++() {
+		this->addGrade(10);
+		return *this;
+	}
+
+	//post
+	GradeBook operator++(int ) {
+		GradeBook copy = *this;
+		this->addGrade(10);
+		return copy;
+	}
+
 	friend void operator>>(istream& console, GradeBook& gradeBook);
 };
 
@@ -137,6 +183,14 @@ GradeBook createAGradeBook() {
 	return newGradeBook;
 }
 
+//alternative for !
+//bool operator!(GradeBook& gradeBook) {
+//	if (gradeBook.getNoGrades() == 0)
+//		return true;
+//	else
+//		return false;
+//}
+
 //overloading operators
 //	1. overload them in the class - they will receive the this pointer as the 1st argument
 //  2. overload them as global methods 
@@ -159,8 +213,49 @@ int main() {
 	cout << oopGradeBook << webGradeBook;
 	//cout << webGradeBook;
 
+	//+=, -=, *=, /=
 								//oopGradeBook = oopGradeBook + 10
 	oopGradeBook += 10;			//operator+=(GradeBook, int)
+	//webGradeBook = oopGradeBook + 10;
 
 
+	// >, <, <=, >=, ==
+	if (oopGradeBook >= webGradeBook) {		//operator>=(GradeBook, GradeBook)
+		cout << endl << oopGradeBook.getName() << " has more grades or the same number of grades as " << webGradeBook.getName();
+	}
+	else {
+		cout << endl << oopGradeBook.getName() << " has less grades than " << webGradeBook.getName();
+	}
+
+	// !
+	if (!oopGradeBook) {		//operator!(Gradebook)
+		cout << endl << oopGradeBook.getName() << " has no grades";
+	}
+	else
+	{
+		cout << endl << oopGradeBook.getName() << " has grades";
+	}
+
+	//cast operator
+	float averageGrade = (float)oopGradeBook;		//operator float(GradeBook)
+	cout << endl << "The average grade is " << averageGrade;
+
+	//using the explicit cast
+	int value = (float) oopGradeBook + 5;
+
+	//++, --
+	int vb1 = 10;
+	int vb2 = ++vb1;
+	int vb3 = vb1++;
+
+	//add a default 10 grade
+	//post incrementation
+	webGradeBook =  oopGradeBook++;		//operator++(GradeBook, int)
+	//preincrementation
+	webGradeBook =  ++oopGradeBook;		//operator++(GradeBook)
+
+	cout << oopGradeBook;
+
+	//operator []
+	//operator function
 }
